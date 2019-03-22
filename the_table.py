3769fea91b28dbaecd43b916b20797e742e8a126
Gino Diaz <gino.diaz@uhc.com>
2019-03-22 08:12:13 -0700

    wk 3/22/2019

--- the_table.py
+++ the_table.py
@@ -40,82 +40,98 @@ def show_round_results(player, dealer):
     player.status += " Total chips:" + str(player.chips.total)
     show_cards(player, dealer)
 
+def continue_play():
+    ans = input('Would you like to play again? Y/N: ')
+    if ans.upper() == 'Y':
+        return True
+
+    return False        
 
 deck = de.Deck()
 #for card in deck.cards:
 #    print(card)
 player1 = de.Player('Cindo',bankroll=200)
-dealer = de.Dealer()
-deck.shuffle()
 player1.turn = True
-print('\n'*100)
+dealer = de.Dealer()
 
 while True:
-    player1.make_bet()
-    if player1.chips.bet > 0:
-        print(player1.chips.bet)
+    
+    deck.shuffle()
+    print('\n'*100)
+
+    while True:
+        player1.make_bet()
+        if player1.chips.bet > 0:
+            print(player1.chips.bet)
+            break
+
+    #-- Initial card dealings
+    card = deck.give_card()
+    card.is_face_up = True
+    player1.hand.add_card(card)
+    card = deck.give_card()
+    dealer.hand.add_card(card)
+    card = deck.give_card()
+    card.is_face_up = True
+    player1.hand.add_card(card)
+    card = deck.give_card()
+    card.is_face_up = True
+    dealer.hand.add_card(card)
+    player1.set_status()
+    dealer.set_status()
+
+    show_cards(player1, dealer)
+    '''
+    print("{a} wagers: {b}".format(a=player1.nick,b=player1.chips.bet))
+    print('Player cards:')
+    dealcards = ''
+    for card in player1.hand.cards:
+        dealcards += card.__str__()
+    print(dealcards)
+    print('Dealer cards:')
+    playercards = ''
+    for card in dealer.hand.cards:
+        playercards += card.__str__()
+    print(playercards)
+    '''
+
+    while player1.turn:
+        if player1.hit_or_stand():
+            player1.hit(deck.give_card())
+            
+        dealer.turn = True if not player1.turn else False
+        show_cards(player1,dealer)
+        if dealer.turn:
+            break
+    #    print('dealer')
+    #    print(dealer.turn )
+    #    print('player')
+    #    print(player1.turn )
+        #quit()
+
+
+    while True: #dealer.turn:
+        if dealer.hand.score < 17:
+            dealer.hit(deck.give_card())
+        else:
+            dealer.set_status()
+        #player1.turn = True if not dealer.turn else False
+
+        dealer.hand.show_all()   
+        show_cards(player1,dealer)
+        time.sleep(3)
+
+        if not dealer.turn:
+            break
+
+    show_round_results(player1, dealer)
+
+    if not continue_play():
         break
-
-#-- Initial card dealings
-card = deck.give_card()
-card.is_face_up = True
-player1.hand.add_card(card)
-card = deck.give_card()
-dealer.hand.add_card(card)
-card = deck.give_card()
-card.is_face_up = True
-player1.hand.add_card(card)
-card = deck.give_card()
-card.is_face_up = True
-dealer.hand.add_card(card)
-player1.set_status()
-dealer.set_status()
-
-show_cards(player1, dealer)
-'''
-print("{a} wagers: {b}".format(a=player1.nick,b=player1.chips.bet))
-print('Player cards:')
-dealcards = ''
-for card in player1.hand.cards:
-    dealcards += card.__str__()
-print(dealcards)
-print('Dealer cards:')
-playercards = ''
-for card in dealer.hand.cards:
-    playercards += card.__str__()
-print(playercards)
-'''
-
-while player1.turn:
-    if player1.hit_or_stand():
-        player1.hit(deck.give_card())
-        
-    dealer.turn = True if not player1.turn else False
-    show_cards(player1,dealer)
-    if dealer.turn:
-        break
-#    print('dealer')
-#    print(dealer.turn )
-#    print('player')
-#    print(player1.turn )
-    #quit()
-
-
-while True: #dealer.turn:
-    if dealer.hand.score < 17:
-        dealer.hit(deck.give_card())
-    else:
-        dealer.set_status()
-    #player1.turn = True if not dealer.turn else False
-
-    dealer.hand.show_all()   
-    show_cards(player1,dealer)
-    time.sleep(3)
-
-    if not dealer.turn:
-        break
-
-show_round_results(player1, dealer)
+    player1.hand.cards.clear()        
+    dealer.hand.cards.clear()   
+    player1.turn = True
+    dealer.turn = False
 
  #   if player1.chips.bet > 0:
  #       player1.turn = False
